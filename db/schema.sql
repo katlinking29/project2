66866f91713c17f754788f5ef624108c702b0726
Jess Moran <jessmoran.dev@gmail.com>
2020-08-07 01:32:26 -0400

    Implements tables and seeds for resources view

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,3 @@
-### Schema
-
 CREATE DATABASE helloWorld_db;
 
 USE helloWorld_db;
@@ -33,5 +31,47 @@ CREATE TABLE event_attendees
     email varchar(100) NOT NULL,
     event_id int NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (event_id) REFERENCES events(id)
+	FOREIGN KEY (event_id) REFERENCES posted_meetups(id)
+);
+
+-- Table to store languages
+CREATE TABLE languages
+(
+    id int NOT NULL AUTO_INCREMENT,
+    language_name varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE videos
+(
+    id int NOT NULL AUTO_INCREMENT,
+    language_id int NOT NULL,
+    name varchar(100) NOT NULL,
+    channel varchar(100) NOT NULL,
+    link varchar(100) NOT NULL,
+  	PRIMARY KEY (id),
+  	FOREIGN KEY (language_id) REFERENCES languages(id)
+);
+
+CREATE TABLE courses
+(
+    id int NOT NULL AUTO_INCREMENT,
+    language_id int NOT NULL,
+    name varchar(100) NOT NULL,
+    path varchar(100) NOT NULL,
+    company_name varchar(100) NOT NULL,
+    link varchar(100) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (language_id) REFERENCES languages(id)
+);
+
+CREATE TABLE books
+(
+    id int NOT NULL AUTO_INCREMENT,
+    language_id int NOT NULL,
+    name varchar(100) NOT NULL,
+    store_url varchar(100) NOT NULL,
+    path varchar(100) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (language_id) REFERENCES languages(id)
 );
