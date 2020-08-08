5f9c5d3c6af61eb79801f0af04bfd66e95c9b7e3
Kyle Johnson <KJohnson3288@yahoo.com>
2020-08-08 11:53:51 -0400

    Event form changes

--- db/schema.sql
+++ db/schema.sql
@@ -8,26 +8,6 @@ CREATE DATABASE helloWorld_db;
 USE helloWorld_db;
 
 
--- table to store user data
-CREATE TABLE users
-(
-	id int NOT NULL AUTO_INCREMENT,
-	email varchar(100) NOT NULL,
-    password varchar(100) NOT NULL,
-	PRIMARY KEY (id)
-);
-
--- table to store networking meetups that users post
-CREATE TABLE events
-(
-	id int NOT NULL AUTO_INCREMENT,
-	event_date DATE NOT NULL,
-    event_time TIME NOT NULL,
-    event_name varchar(100) NOT NULL,
-    event_location varchar(200) NOT NULL,
-    event_description varchar(200) NOT NULL,
-	PRIMARY KEY (id)
-);
 
 CREATE TABLE event_attendees
 (
