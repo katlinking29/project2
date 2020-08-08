d664fa409d29c9bbaecbfec9f8e5af77232fa316
Katlin King <katlinking29@gmail.com>
2020-08-08 11:48:40 -0400

    updated event model to match the form in the dashboard.handlebars file

--- models/event.js
+++ models/event.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   const Event = sequelize.define("events", {
     event_name: DataTypes.STRING,
-    event_date: DataTypes.STRING,
+    event_date: DataTypes.DATE,
     event_city: DataTypes.STRING,
     event_state: DataTypes.STRING,
     event_zip: DataTypes.STRING
