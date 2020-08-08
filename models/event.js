bf835fb0528a0c9f38c1e0e8ae9ad6477f942bba
Katlin King <katlinking29@gmail.com>
2020-08-08 14:08:27 -0400

    fixed capitalizatino errors in the event model, updated the event-routes-js file so events successfully post to the database, and added a name value to the dashboard.handlebars file

--- models/event.js
+++ models/event.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
-  const Event = sequelize.define("events", {
-    event_name: DataTypes.STRING,
-    event_date: DataTypes.DATE,
-    event_city: DataTypes.STRING,
-    event_state: DataTypes.STRING,
-    event_zip: DataTypes.STRING
+  const Event = sequelize.define("Event", {
+    name: DataTypes.STRING,
+    date: DataTypes.DATE,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zip: DataTypes.STRING
   });
   return Event;
 };
